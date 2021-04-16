6acce909959847b711346bca6b2dd38dfb5003b3
Stefania Makrygiannaki <stefania-mak-@hotmail.com>
2021-04-16 21:17:59 +0300

    modified UI

--- lib/main.dart
+++ lib/main.dart
@@ -47,68 +47,80 @@ class _MyHomePageState extends State<MyHomePage> {
     // );
     Color _darkMainColor = Colors.white10;
     Color _lightMainColor = Colors.white;
+    double windowWidth = MediaQuery.of(context).size.width;
+    print('windowWidth: $windowWidth');
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: _darkMainColor,
-        body: SafeArea(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              CircleAvatar(
-                radius: 50.0,
-                backgroundImage: AssetImage('assets/stefania_mak_icon.png'),
-              ),
-              Text(
-                'Stefania Mak',
-                style: TextStyle(
-                    fontFamily: 'Grenze',
-                    fontSize: 40.0,
-                    fontWeight: FontWeight.bold,
-                    color: _lightMainColor),
-              ),
-              Text(
-                'Android App Developer',
-                style: TextStyle(
-                    fontFamily: 'Turret',
-                    fontSize: 15.0,
-                    color: _lightMainColor,
-                    letterSpacing: 2.5),
-              ),
-              SizedBox(
-                height: 20.0,
-                width: 110.0,
-                child: Divider(
-                  color: Colors.grey[200],
+        backgroundColor: Color(0xFF1b1c1c),
+        body: Center(
+          child: Container(
+            width: windowWidth > 800 ? 800.0 : windowWidth,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                CircleAvatar(
+                  radius: 50.0,
+                  backgroundImage: AssetImage('assets/stefania_mak_icon.png'),
                 ),
-              ),
-              Card(
-                margin: EdgeInsets.symmetric(horizontal: 80.0, vertical: 13.0),
-                color: _lightMainColor,
-                child: ListTile(
-                    leading: Icon(
-                      Icons.phone,
-                      color: Colors.black,
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 15),
+                  child: Text(
+                    'Stefania Mak',
+                    style: TextStyle(
+                        // fontFamily: 'Grenze',
+                        fontSize: 25.0,
+                        fontWeight: FontWeight.bold,
+                        color: _lightMainColor),
+                  ),
+                ),
+                Text(
+                  'Flutter App Developer',
+                  style: TextStyle(
+                      // fontFamily: 'Turret',
+                      fontSize: 15.0,
+                      color: _lightMainColor,
+                      letterSpacing: 2.5),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(25.0),
+                  child: SizedBox(
+                    height: 20.0,
+                    // width: 110.0,
+                    child: Divider(
+                      color: Colors.grey[200],
                     ),
-                    title: Text(
-                      '+30 123 456 7890',
-                      style: TextStyle(color: Colors.black),
-                    )),
-              ),
-              Card(
-                margin: EdgeInsets.symmetric(horizontal: 80.0),
-                color: _lightMainColor,
-                child: ListTile(
-                    leading: Icon(
-                      Icons.email,
-                      color: Colors.black,
-                    ),
-                    title: Text(
-                      'aaa@gmail.com',
-                      style: TextStyle(color: Colors.black),
-                    )),
-              ),
-            ],
+                  ),
+                ),
+                // Card(
+                //   margin:
+                //       EdgeInsets.symmetric(horizontal: 80.0, vertical: 13.0),
+                //   color: _lightMainColor,
+                //   child: ListTile(
+                //       leading: Icon(
+                //         Icons.phone,
+                //         color: Colors.black,
+                //       ),
+                //       title: Text(
+                //         '+30 123 456 7890',
+                //         style: TextStyle(color: Colors.black),
+                //       )),
+                // ),
+                Card(
+                  margin: EdgeInsets.symmetric(horizontal: 80.0),
+                  color: _lightMainColor,
+                  child: ListTile(
+                      leading: Icon(
+                        Icons.email,
+                        color: Colors.black,
+                      ),
+                      title: Text(
+                        'stefania.mak.project@hotmail.com',
+                        style: TextStyle(color: Colors.black),
+                      )),
+                ),
+              ],
+            ),
           ),
         ),
       ),
