396ceac0c5fe1cb6b8bf36ecfe755065a6994412
stefaniamak <stefania-mak-@hotmail.com>
2021-08-28 10:43:19 +0200

    refactor: places link button to its own widget

--- lib/pages/link_tree/widgets/links_list.dart
+++ lib/pages/link_tree/widgets/links_list.dart
@@ -1,10 +1,9 @@
-// ignore: avoid_web_libraries_in_flutter
-import 'dart:html' as html;
-
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:stefaniamak/ui_kit/styles/colors.dart';
+import 'package:stefaniamak/constants/constants.dart';
 import 'package:stefaniamak/view_models/button_view_model.dart';
 
+import '../link_button.dart';
 import '../link_tree.dart';
 
 class LinksList extends StatelessWidget {
@@ -15,7 +14,6 @@ class LinksList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    double buttonHeight = 60.0;
     return Container(
       height: buttonViewModelList.length * (buttonHeight + 20.0),
       width:
@@ -23,41 +21,7 @@ class LinksList extends StatelessWidget {
       child: Column(
         children: [
           ...buttonViewModelList.map(
-            (button) => Row(
-              children: [
-                Expanded(
-                  child: Padding(
-                    padding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
-                    child: Material(
-                      child: InkWell(
-                        onTap: () {
-                          // import 'dart:js' as js;
-                          // js.context.callMethod('open',
-                          //     ['https://stackoverflow.com/questions/ask']);
-
-                          // import 'dart:html' as html;
-                          // String url = 'https://flutter.dev';
-                          // if (button.link != '')
-                          html.window.open(button.link,
-                              '_blank'); // for new '_blank' for same '_self'
-                        },
-                        child: Container(
-                          color: kLightMainColor,
-                          height: buttonHeight,
-                          child: Center(
-                            child: Text(
-                              button.title,
-                              style: TextStyle(
-                                  fontSize: 16, fontWeight: FontWeight.bold),
-                            ),
-                          ),
-                        ),
-                      ),
-                    ),
-                  ),
-                )
-              ],
-            ),
+            (ButtonViewModel button) => LinkButton(buttonVm: button),
           )
         ],
       ),
