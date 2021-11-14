238733ca5a87c79340dbac5ce192bfeb2b9e4e66
Stefania Makrygiannaki <stefania-mak-@hotmail.com>
2021-11-15 00:27:54 +0200

    adds smooth scrolling to homepage

--- lib/pages/my_page.dart
+++ lib/pages/my_page.dart
@@ -1,16 +1,25 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:stefaniamak/main.dart';
 
 class MyPage extends StatelessWidget {
   static const String route = '/';
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        height: MediaQuery.of(context).size.height,
-        width: double.infinity,
-        color: Colors.white,
-        child: Center(child: Text('Under Construction.')),
+    var controller = ScrollController();
+    return PageSetup(
+      controller: controller,
+      child: SingleChildScrollView(
+        controller: controller,
+        physics:
+            kIsWeb ? NeverScrollableScrollPhysics() : BouncingScrollPhysics(),
+        child: Container(
+          height: MediaQuery.of(context).size.height + 500,
+          width: double.infinity,
+          color: Colors.white,
+          child: Center(child: Text('Under Construction.')),
+        ),
       ),
     );
   }
