b34ea73a809516dbef00e082177b4cd75ab524ea
Stefania Makrygiannaki <stefania-mak-@hotmail.com>
2021-11-15 02:38:12 +0200

    custom parallax page

--- lib/pages/my_page.dart
+++ lib/pages/my_page.dart
@@ -2,257 +2,190 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:stefaniamak/ui_kit/smooth_scrolling.dart';
 
-class MyPage extends StatelessWidget {
+class MyPage extends StatefulWidget {
   static const String route = '/';
 
   @override
-  Widget build(BuildContext context) {
-    var controller = ScrollController();
-    return ParallaxPage();
-    return Scaffold(
-      body: SmoothScrolling(
-        controller: controller,
-        child: SingleChildScrollView(
-          controller: controller,
-          physics:
-              kIsWeb ? NeverScrollableScrollPhysics() : BouncingScrollPhysics(),
-          child: Container(
-            height: MediaQuery.of(context).size.height + 500,
-            width: double.infinity,
-            color: Colors.white,
-            child: Center(child: Text('Under Construction.')),
-          ),
-        ),
-      ),
-    );
-  }
+  _MyPageState createState() => _MyPageState();
 }
 
-class ParallaxPage extends StatefulWidget {
-  final Widget rest;
-
-  const ParallaxPage({Key key, this.rest}) : super(key: key);
-
-  @override
-  _ParallaxPageState createState() => _ParallaxPageState();
-}
-
-class _ParallaxPageState extends State<ParallaxPage> {
-  double pageScrollValue = 0;
-  double letterValue = 100;
+class _MyPageState extends State<MyPage> {
   var controller = ScrollController();
-
-  @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-
-    controller.addListener(() {
-      // setState(() {
-      pageScrollValue = controller.position.pixels;
-      // });
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
     var md = MediaQuery.of(context);
-    // pageScrollValue = controller.position.pixels;
-    return Scaffold(
-      body: NotificationListener(
-        onNotification: (v) {
-          // todo
-          if (v is ScrollUpdateNotification) {
-            // setState(() {
-            // pageScrollValue = pageScrollValue - v.scrollDelta! / 3;
-            // pageScrollValue = pageScrollValue - v.scrollDelta;
-            // pageScrollValue = controller.position.pixels;
-            // controller.jumpTo(pageScrollValue);
-            print('controller.position.pixels: ${controller.position.pixels}');
-            // });
-          }
+    return ParallaxPage(
+      controller: controller,
+      behindParallaxes: buildTopBehindParallax(md),
+      staticPage: buildStaticLetters(md),
+      frontPage: buildFrontPage(md.size),
+    );
+  }
 
-          return true;
-        },
-        child: SmoothScrolling(
-          controller: controller,
-          child: Stack(
-            children: [
-              Test(
-                md: md,
-                controller: controller,
-              ),
-//               Positioned(
-//                 left: 0,
-//                 right: 0,
-//                 top: pageScrollValue + md.size.height * 0.45,
-//                 child: Container(
-//                   height: md.size.height * 1.2,
-//                   color: Colors.white,
-//                   child: Center(
-//                     child: Row(
-//                       children: [
-//                         SizedBox(
-//                           width: md.size.width * 0.1,
-//                         ),
-//                         Text(
-//                           '''
-// We are Dynamic;
-// a photography & design
-// studio that prides itself on
-// high contrast works from all walks of culture,
-// background, and life.''',
-//                           style: TextStyle(
-//                             fontSize: (md.size.width / 6).clamp(16, 46),
-//                             height: 0.85,
-//                             color: Colors.grey,
-//                             fontWeight: FontWeight.bold,
-//                           ),
-//                         ),
-//                       ],
-//                     ),
-//                   ),
-//                 ),
-//               ),
-              Positioned(
-                right: md.size.width * 0.125,
-                top: md.size.height * 0.4,
-                child: Text(
-                  '''Dynamic
-                      Studios'''
-                      .toUpperCase(),
-                  style: TextStyle(
-                    fontSize: (md.size.width / 6).clamp(70, 150),
-                    height: 0.85,
-                    fontWeight: FontWeight.bold,
-                  ),
-                  textAlign: TextAlign.right,
-                ),
-              ),
+  Positioned buildStaticLetters(MediaQueryData md) {
+    return Positioned(
+      right: md.size.width * 0.125,
+      top: md.size.height * 0.58 - 150,
+      child: Text(
+        '''Stefania
+                  Mak'''
+            .toUpperCase(),
+        style: TextStyle(
+          fontSize: (md.size.width / 6).clamp(70, 150),
+          height: 0.85,
+          fontWeight: FontWeight.bold,
+        ),
+        textAlign: TextAlign.right,
+      ),
+    );
+  }
 
-              // Positioned(
-              //   left: 0,
-              //   top: pageScrollValue + 50,
-              //   child: Container(
-              //     width: md.size.width,
-              //     height: 100,
-              //     color: Colors.green,
-              //   ),
-              // ),
-
-              // Container(
-              //   height: md.size.height * 0.45 + md.size.height * 0.9,
-              // ),
-              // // Container(
-              // //   height: md.size.height,
-              // //   color: Colors.white,
-              // //   child: Text('This is a test '),
-              // // ),
-              // Container(
-              //   height: md.size.height * 0.8,
-              //   child: Image.network(
-              //     'https://scontent.fskg3-1.fna.fbcdn.net/v/t1.6435-9/227779153_4114045565297432_1171332811753549550_n.jpg?_nc_cat=103&ccb=1-5&_nc_sid=730e14&_nc_ohc=s8bOaB5GmmoAX9WzQiE&tn=kMVdjguRRGkPK8CN&_nc_ht=scontent.fskg3-1.fna&oh=25fd5ca17911c0f783b316ee32a04889&oe=619F6CC2',
-              //     height: md.size.height * 0.45,
-              //     width: md.size.width,
-              //     fit: BoxFit.cover,
-              //   ),
-              // ),
-              // Container(
-              //   height: md.size.height * 0.4,
-              //   color: Colors.white.withOpacity(0.7),
-              // ),
-              // Container(
-              //   height: md.size.height * 5,
-              //   color: Colors.white,
-              // ),
-              ListView(
-                controller: controller,
-                physics: kIsWeb
-                    ? NeverScrollableScrollPhysics()
-                    : BouncingScrollPhysics(),
+  ChildParallax buildTopBehindParallax(MediaQueryData md) {
+    return ChildParallax(
+      controller: controller,
+      child: Column(
+        children: [
+          Opacity(
+            opacity: 0.5,
+            child: Image.network(
+              'https://scontent.fskg3-1.fna.fbcdn.net/v/t1.6435-9/223167117_4114046371964018_809327451424539040_n.jpg?_nc_cat=100&ccb=1-5&_nc_sid=730e14&_nc_ohc=vxNuTWMPk3EAX_eRCP9&_nc_ht=scontent.fskg3-1.fna&oh=56682216b2d11bb2c32d1af6b2f5f27d&oe=619D8840',
+              height: md.size.height * 0.58,
+              width: md.size.width,
+              fit: BoxFit.cover,
+            ),
+          ),
+          Container(
+            height: md.size.height * 1.2,
+            width: md.size.width,
+            color: Colors.white,
+            child: Center(
+              child: Row(
                 children: [
-                  Container(
-                    height: md.size.height * 0.45 + md.size.height * 0.9,
+                  SizedBox(
+                    width: md.size.width * 0.1,
                   ),
-                  // Container(
-                  //   height: md.size.height,
-                  //   color: Colors.white,
-                  //   child: Text('This is a test '),
-                  // ),
-                  Container(
-                    height: md.size.height * 0.8,
-                    child: Image.network(
-                      'https://scontent.fskg3-1.fna.fbcdn.net/v/t1.6435-9/227779153_4114045565297432_1171332811753549550_n.jpg?_nc_cat=103&ccb=1-5&_nc_sid=730e14&_nc_ohc=s8bOaB5GmmoAX9WzQiE&tn=kMVdjguRRGkPK8CN&_nc_ht=scontent.fskg3-1.fna&oh=25fd5ca17911c0f783b316ee32a04889&oe=619F6CC2',
-                      height: md.size.height * 0.45,
-                      width: md.size.width,
-                      fit: BoxFit.cover,
+                  Text(
+                    '''
+We are Dynamic;
+a photography & design
+studio that prides itself on
+high contrast works from all walks of culture,
+background, and life.''',
+                    style: TextStyle(
+                      fontSize: (md.size.width / 6).clamp(16, 46),
+                      height: 0.85,
+                      color: Colors.grey,
+                      fontWeight: FontWeight.bold,
                     ),
                   ),
-                  Container(
-                    height: md.size.height * 0.4,
-                    color: Colors.white.withOpacity(0.7),
-                  ),
-                  Container(
-                    height: md.size.height * 5,
-                    color: Colors.white,
-                  ),
                 ],
-              )
-            ],
+              ),
+            ),
           ),
+        ],
+      ),
+    );
+  }
+
+  List<Widget> buildFrontPage(Size mdSize) {
+    return [
+      Container(
+        height: mdSize.height * 0.45 + mdSize.height * 0.9,
+      ),
+      Container(
+        height: mdSize.height * 0.8,
+        child: Image.network(
+          'https://scontent.fskg3-1.fna.fbcdn.net/v/t1.6435-9/227779153_4114045565297432_1171332811753549550_n.jpg?_nc_cat=103&ccb=1-5&_nc_sid=730e14&_nc_ohc=s8bOaB5GmmoAX9WzQiE&tn=kMVdjguRRGkPK8CN&_nc_ht=scontent.fskg3-1.fna&oh=25fd5ca17911c0f783b316ee32a04889&oe=619F6CC2',
+          height: mdSize.height * 0.45,
+          width: mdSize.width,
+          fit: BoxFit.cover,
+        ),
+      ),
+      Container(
+        height: mdSize.height * 0.4,
+        color: Colors.white.withOpacity(0.7),
+      ),
+      Container(
+        height: mdSize.height * 5,
+        color: Colors.white,
+      ),
+    ];
+  }
+}
+
+class ParallaxPage extends StatelessWidget {
+  final ScrollController controller;
+  final ChildParallax behindParallaxes;
+  final List<Widget> frontPage;
+  final ChildParallax frontParallaxes;
+  final Positioned staticPage;
+
+  const ParallaxPage({
+    @required this.controller,
+    this.behindParallaxes,
+    this.frontPage = const [],
+    this.frontParallaxes,
+    Key key,
+    @required this.staticPage,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    var md = MediaQuery.of(context);
+    return Scaffold(
+      body: SmoothScrolling(
+        controller: controller,
+        child: Stack(
+          children: [
+            if (behindParallaxes != null) behindParallaxes,
+            staticPage,
+            ListView(
+              controller: controller,
+              physics: kIsWeb
+                  ? NeverScrollableScrollPhysics()
+                  : BouncingScrollPhysics(),
+              children: frontPage,
+            ),
+            if (frontParallaxes != null) frontParallaxes,
+          ],
         ),
       ),
     );
   }
 }
 
-class Test extends StatefulWidget {
-  const Test({
+class ChildParallax extends StatefulWidget {
+  const ChildParallax({
     Key key,
-    @required this.md,
-    this.controller,
+    @required this.controller,
+    @required this.child,
+    this.speed = 1.0,
   }) : super(key: key);
 
-  final MediaQueryData md;
   final ScrollController controller;
+  final Widget child;
+  final double speed;
 
   @override
-  _TestState createState() => _TestState();
+  _ChildParallaxState createState() => _ChildParallaxState();
 }
 
-class _TestState extends State<Test> {
+class _ChildParallaxState extends State<ChildParallax> {
   var pageScrollValue = 0.0;
+
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
-
     widget.controller.addListener(() {
       setState(() {
-        pageScrollValue = -widget.controller.position.pixels;
+        pageScrollValue = -widget.controller.position.pixels * widget.speed;
       });
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    return Positioned(
-      left: 0,
-      top: pageScrollValue,
-      child: Opacity(
-        opacity: 0.5,
-        child: Image.network(
-          'https://scontent.fskg3-1.fna.fbcdn.net/v/t1.6435-9/223167117_4114046371964018_809327451424539040_n.jpg?_nc_cat=100&ccb=1-5&_nc_sid=730e14&_nc_ohc=vxNuTWMPk3EAX_eRCP9&_nc_ht=scontent.fskg3-1.fna&oh=56682216b2d11bb2c32d1af6b2f5f27d&oe=619D8840',
-          height: widget.md.size.height * 0.45,
-          width: widget.md.size.width,
-          fit: BoxFit.cover,
-        ),
-      ),
-      // child: Container(
-      //   width: md.size.width,
-      //   height: 100,
-      //   color: Colors.red,
-      // ),
-    );
+    return Positioned(left: 0, top: pageScrollValue, child: widget.child);
   }
 }
---
+++ lib/ui_kit/pages/under_construction.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:stefaniamak/ui_kit/smooth_scrolling.dart';
+
+class UnderConstruction extends StatelessWidget {
+  const UnderConstruction({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    var controller = ScrollController();
+    return Scaffold(
+      body: SmoothScrolling(
+        controller: controller,
+        child: SingleChildScrollView(
+          controller: controller,
+          physics:
+              kIsWeb ? NeverScrollableScrollPhysics() : BouncingScrollPhysics(),
+          child: Container(
+            height: MediaQuery.of(context).size.height,
+            width: double.infinity,
+            color: Colors.white,
+            child: Center(child: Text('Under Construction.')),
+          ),
+        ),
+      ),
+    );
+  }
+}
